f25c4a5094cdd47c6fbbb9aa7c5103448dcb798e
Cassius Naylor <cassiusnaylor@MASTUDENT-Cassius-Naylor-C02G5H2RQ6L4.local>
2023-03-21 16:37:00 +0000

    2. Wrote first tests to confirm expected behaviour of existing code

--- js-jest/plan.md
+++ js-jest/plan.md
@@ -4,10 +4,11 @@ The Shop class has a method called updateQuality() which iterates through each I
 
 - For items that are not 'Aged Brie' or 'Backstage passes to a TAFKAL80ETC concert', the quality is decreased by 1 if it is greater than 0, except for 'Sulfuras, Hand of Ragnaros' which has a fixed quality of 80 and never changes.
 - For 'Aged Brie' and 'Backstage passes to a TAFKAL80ETC concert', the quality is increased by 1 if it is less than 50. Additionally, for 'Backstage passes', the quality is increased by an additional 1 or 2 if the sellIn value is less than 11 or 6 days, respectively.
+
 - The sellIn value for all items, except for 'Sulfuras, Hand of Ragnaros', is decreased by 1.
 - If the sellIn value for an item has passed (is less than 0), the quality of the item is updated based on its type:
     - For items that are not 'Aged Brie' or 'Backstage passes', the quality is decreased by 1 if it is greater than 0, except for 'Sulfuras, Hand of Ragnaros' which has a fixed quality of 80 and never changes.
     - For 'Aged Brie', the quality is increased by 1 if it is less than 50.
     - For 'Backstage passes', the quality is set to 0.
-    
+
 Finally, the method returns the updated array of Item objects.
--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -5,7 +5,31 @@ describe("Gilded Rose", function() {
     const gildedRose = new Shop([new Item("foo", 6, 2)]);
     const items = gildedRose.updateQuality();
     expect(items[0].name).toBe("foo");
-    expect(items[0].quality).toBe(1)
     expect(items[0].sellIn).toBe(5)
+    expect(items[0].quality).toBe(1)
+  });
+
+  it("should read back qualities of aged brie with improved quality", function() {
+    const gildedRose = new Shop([new Item("Aged Brie", 6, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Aged Brie");
+    expect(items[0].sellIn).toBe(5)
+    expect(items[0].quality).toBe(3)
+  });
+
+  it("should read back qualities of a backstage pass with doubled quality", function() {
+    const gildedRose = new Shop([new Item("Backstage passes to a TAFKAL80ETC concert", 6, 2)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+    expect(items[0].sellIn).toBe(5)
+    expect(items[0].quality).toBe(4)
+  });
+
+  it("should read back qualities of Sulfuras with static quality", function() {
+    const gildedRose = new Shop([new Item("Sulfuras, Hand of Ragnaros", 6, 80)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+    expect(items[0].sellIn).toBe(6)
+    expect(items[0].quality).toBe(80)
   });
 });
